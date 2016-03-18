2b00dcf5a2a43eb050f434a18294b042b91772f5
kickscar <kickscar@gmail.com>
2016-03-18 12:44:29 +0900

    포맷팅

--- 기본[hr계정_SQL_실습문제]_SOL.sql
+++ 기본[hr계정_SQL_실습문제]_SOL.sql
@@ -1,45 +1,45 @@
 -- 문제1
-  SELECT CONCAT( first_name, ' ', last_name) AS '이름'
-   FROM employees
-  WHERE emp_no = 10944;
+SELECT CONCAT( first_name, ' ', last_name) AS '이름'
+  FROM employees
+ WHERE emp_no = 10944;
    
 -- 문제2
-     SELECT CONCAT( first_name, ' ', last_name) AS '이름', gender AS '성별', hire_date AS '입사일'
-      FROM employees
+  SELECT CONCAT( first_name, ' ', last_name) AS '이름', gender AS '성별', hire_date AS '입사일'
+    FROM employees
 ORDER BY hire_date ASC;
 
 -- 문제3
-  SELECT COUNT(*)
-   FROM employees
+SELECT COUNT(*)
+  FROM employees
  WHERE gender = 'm';
 
-  SELECT COUNT(*)
-   FROM employees
+SELECT COUNT(*)
+  FROM employees
  WHERE gender = 'f';
 
 --문제4
-  SELECT COUNT(*)
-   FROM salaries
+SELECT COUNT(*)
+  FROM salaries
  WHERE to_date = '9999-1-1';
  
 --문제5
-  SELECT count(*)
-   FROM departments;
+SELECT COUNT(*)
+  FROM departments;
 
 --문제6
-   SELECT count(*)
-    FROM dept_manager
-  WHERE to_date = '9999-1-1';
+SELECT count(*)
+  FROM dept_manager
+ WHERE to_date = '9999-1-1';
    
 --문제7
-     SELECT dept_name
-      FROM departments
+  SELECT dept_name
+    FROM departments
 ORDER BY LENGTH(dept_name) DESC;
 
 --문제8
- SELECT count(*)
+SELECT count(*)
   FROM salaries
-WHERE to_date = '9999-1-1'
+ WHERE to_date = '9999-1-1'
    AND salary > 120000;    
 
 --문제9
